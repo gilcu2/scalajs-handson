4e1712366a54396521c8624a78b910b77d4d29b3
Reynaldo <gilcu2@gmail.com>
2015-04-22 12:49:30 +0200

    sigma dont working

--- src/main/scala/sigmaScala/test.scala
+++ src/main/scala/sigmaScala/test.scala
@@ -9,20 +9,21 @@ import scalajs.js.annotation.JSExport
 @JSExport
 object UI {
   @JSExport
-  def main(target: html.Div) = {
+  def main(target: html.Div):Unit = {
 
     import js.Dynamic.{ global => jsGlo, newInstance => jsNew ,literal=>jsLit}
 
-    val s =  jsNew(jsGlo.sigma)(
-        jsLit(container="full_window"))
+    val s =  jsNew(jsGlo.sigma)("container")
 
     val node0=jsLit(id="n0", label="Hello", x=0,y= 0,size=1, color="#f00")
-    val node1=jsLit(id="n1", label="World", x=1,y= 1,size=1, color="#f00")
-    val edge=jsLit(id="e0", source="n0",tarjet="n1")
+    val node1=jsLit(id="n1", label="World", x=1,y= 1,size=1, color="#00f")
+    val edge=jsLit(id="e0", source="n0",target="n1")
 
-    s.addNode(node0).addNode(node1).addEdge(edge)
+    s.graph.addNode(node0)
+    s.graph.addNode(node1)
+    s.graph.addEdge(edge)
 
-    s.refresh
+    s.refresh()
 
 
   }
